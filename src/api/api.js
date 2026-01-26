597c5456b1e9cd88c3a851c2ed38cf1ae80f232f
ViniAvemaria <160148129+ViniAvemaria@users.noreply.github.com>
2026-01-26 16:18:37 -0300

    feat: change api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://coretech-project-backend.onrender.com",
     withCredentials: true,
 });
 
@@ -23,7 +23,7 @@ api.interceptors.response.use(
         }
 
         return Promise.reject(err);
-    }
+    },
 );
 
 export default api;
