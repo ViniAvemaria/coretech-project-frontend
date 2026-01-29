97cc3bde3b62f443d6fbc7486b6008c4ddf6c82f
ViniAvemaria <160148129+ViniAvemaria@users.noreply.github.com>
2026-01-29 12:06:53 -0300

    feat: add env var to change backend url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-    baseURL: "https://coretech-project-backend.onrender.com/api",
+    baseURL: API_URL,
     withCredentials: true,
 });
 
